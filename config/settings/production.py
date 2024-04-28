da8796471ca02362fbdb30b278f18ecf94c339bc
sudarshan mestha <112337032+sudarshanmestha@users.noreply.github.com>
2024-04-28 22:48:08 +0530

    Update production.py

--- config/settings/production.py
+++ config/settings/production.py
@@ -13,7 +13,7 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["jobs.justdjango.com"])
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["jobs.justpython.in,django-jobs.captain.justpython.in"])
 
 # DATABASES
 # ------------------------------------------------------------------------------
